c4baea2e897da00c5cb16f7700dd94d6a38c6bfc
Josue <jdd1993@hotmail.com>
2022-10-16 18:24:48 -0400

    add JS functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /*GAME*/
 
 /*
-My game is going to play against the computer, so I begin with a function 
+I'm going to play against the computer, so I begin with a function 
 called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 I'll use this function in the game to make the computer’s play.
 */
@@ -14,39 +14,35 @@ function getComputerChoice () {
     let ComputerDecision = "";
     
     if (x == 1) {
-        ComputerDecision = "rock";
+        ComputerDecision = "Rock";
     } else if (x == 2) {
-        ComputerDecision = "paper";
+        ComputerDecision = "Paper";
     } else if (x == 3) {
-        ComputerDecision = "scissors";
+        ComputerDecision = "Scissors";
     }
 
     return ComputerDecision;
 }
 
-/*
-Write a function that plays a single round of Rock Paper Scissors. 
-The function should take two parameters - the playerSelection and 
-computerSelection and then return a string that declares the winner
- of the round like so: "You Lose! Paper beats Rock"*/
 
+//This function will compare my selection with the computer's.
 
 function playRound(playerSelection, a = computerSelection) {
     
-    let b = playerSelection.toLowerCase();
+    let b = playerSelection;
     let refereeDecision = "";
         
-    if (b == "rock" && a == "paper") {
+    if (b == "Rock" && a == "Paper") {
         refereeDecision = "pc";
-    } else if (b == "paper" && a == "rock") {
+    } else if (b == "Paper" && a == "Rock") {
         refereeDecision = "user";
-    }else if (b == "rock" && a == "scissors") {
+    }else if (b == "Rock" && a == "Scissors") {
         refereeDecision = "user";
-    }else if (b == "scissors" && a == "rock") {
+    }else if (b == "Scissors" && a == "Rock") {
         refereeDecision = "pc";
-    }else if (b == "paper" && a == "scissors") {
+    }else if (b == "Paper" && a == "Scissors") {
         refereeDecision = "pc";
-    }else if (b == "scissors" && a == "paper") {
+    }else if (b == "Scissors" && a == "Paper") {
         refereeDecision = "user";
     }
 
@@ -54,60 +50,8 @@ function playRound(playerSelection, a = computerSelection) {
 
 }
 
-/*
-Write a NEW function called game(). Call the playRound function inside of this one to play a 5 
-round game that keeps score and reports a winner or loser at the end.
-*/
+//The function below will decide who is going to be the winner.
 
-
-function game() {
-
-    let user = 0;
-    let pc = 0;
-    let result = ``;
-    let  checkScenerio = false;
-
-    
-
-    while (checkScenerio == false){
-
-        let playerSelection = prompt("enter rock, paper or scissors;", );
-        let computerSelection = getComputerChoice();
-        let random = playRound(playerSelection,computerSelection);
-
-        if (random == "user") {
-            user++;
-            console.log("you won!")
-        } else if (random == "pc") {
-            pc++;
-            console.log("The pc won!")
-        } else if (random = "draw") {
-            console.log("There was a draw!")
-        }
-
-        /*
-        if (user >= 5 && user > pc) {
-            result = `you are the winner, check the scores: you:${user.toString()}, computer:${pc.toString()}`;
-        } else if (pc >= 5 && pc > user){
-            result = `the computer is the winner, check the scores: you:${user.toString()}, computer:${pc.toString()}`;
-        } 
-        */
-
-        if (user >= 5 && user > pc) {
-            result = `you are the winner, check the scores: you:${user}, computer:${pc}`;
-        } else if (pc >= 5 && pc > user){
-            result = `the computer is the winner, check the scores: you:${user}, computer:${pc}`;
-        } 
-        
-        if ((user == 5) || (pc == 5)) {
-            checkScenerio = true;
-        }
-
-    }
-
-    return result;
-
-}
 
 
 
@@ -117,21 +61,22 @@ const gameBox = document.querySelector('#game');
 const screen = document.createElement('div');
 screen.classList.add('screen');
 const title1 = document.createElement('p');
+title1.textContent = 'You have 5 rounds to play. your score will show on this screen!';
 const startBtn = document.createElement('button');
 startBtn.textContent = "Start";
 startBtn.classList.add('startBtn');
 
 
-title1.textContent = 'You have 5 rounds to play. your score will show on this screen!';
+
 const screenSubtitle = document.createElement('div');
 screenSubtitle.classList.add('screenSubtitle');
 const title2 = document.createElement('p');
 title2.textContent = 'Choose your weapon!';
 const screenBtns = document.querySelector('div[class="btnsList"]');
 //buttons
-const screenBtn1 = document.querySelector('button[class="screenBtns"]');
-const screenBtn2 = document.querySelector('button[class="screenBtns"]');
-const screenBtn3 = document.querySelector('button[class="screenBtns"]');
+const screenBtn1 = document.querySelector('button[id="screenBtn1"]');
+const screenBtn2 = document.querySelector('button[id="screenBtn2"]');
+const screenBtn3 = document.querySelector('button[id="screenBtn3"]');
 
 //Iterate through all the buttons
 const btnList = document.querySelectorAll('button[class="screenBtns"]');
@@ -159,24 +104,70 @@ function showGame(){
 
 }
 
-
 function showGame2(){
 
     gameBox.removeChild(startBtn);
     screen.removeChild(title1);
     screenSubtitle.appendChild(title2);
     gameBox.appendChild(screenBtns);
-    console.log(btnListArr);
 
 }
 
 
+    let user = 0;
+    let pc = 0;
+    let playerSelection = "";
+
+//This function compares the user selection and the computer selection
 function revelation(e){
-    //console.log(typeof(e.target.innerText))
-    console.log(e.target.innerText)
+
+    let computerSelection = getComputerChoice();
+
+    //console.log(e.target.innerText);
+    if(e.target.innerText == "Rock" && computerSelection == "Paper"){
+        pc++;
+        console.log("pc");
+    } else if(e.target.innerText == "Paper" && computerSelection == "Rock"){
+        user++;
+        console.log("user");
+    } else if(e.target.innerText == "Rock" && computerSelection == "Scissors"){
+        user++;
+        console.log("user");
+    } else if(e.target.innerText == "Paper" && computerSelection == "Scissors"){
+        pc++;
+        console.log("pc");
+    }else if(e.target.innerText == "Scissors" && computerSelection == "Paper"){
+        user++;
+        console.log("user");
+    }else if(e.target.innerText == "Scissors" && computerSelection == "Rock"){
+        pc++;
+        console.log("pc");
+    }else if(e.target.innerText == computerSelection){
+        console.log('There is a draw!');
+    }
+    //Once one of the players get 5 points the buttons disappear
+    if (user == 5) {
+        console.log("You won!");
+        gameBox.removeChild(screenBtns);
+    }if (pc == 5) {
+        console.log("The PC won!");
+        gameBox.removeChild(screenBtns);
+    }
+    
+    //e.stopPropagation()
+
 }
+
+
+
+
+
 
 
 btn.addEventListener('click', showGame);
 startBtn.addEventListener('click', showGame2);
-btnListArr.forEach(btnListArr => btnListArr.addEventListener('click', revelation));
+screenBtn1.addEventListener('click', revelation);
+screenBtn2.addEventListener('click', revelation);
+screenBtn3.addEventListener('click', revelation);
+
+
