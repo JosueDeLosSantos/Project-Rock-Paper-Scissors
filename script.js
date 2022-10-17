f5cad617d7e3b0c69e524cc6794e3a7e0d4ca8a6
Josue <jdd1993@hotmail.com>
2022-10-17 14:01:16 -0400

    final project

--- script.js
+++ script.js
@@ -2,12 +2,75 @@
 
 /*GAME*/
 
-/*
-I'm going to play against the computer, so I begin with a function 
-called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-I'll use this function in the game to make the computer’s play.
-*/
+const mainSelector = document.querySelector('main');
+const btn = document.querySelector('#button');
+const gameBox = document.querySelector('#game');
+const startBtn = document.createElement('button');
+startBtn.textContent = "Start";
+startBtn.classList.add('startBtn');
 
+//screen
+const screen = document.createElement('div');
+screen.classList.add('screen');
+const title1 = document.createElement('p');
+title1.textContent = 'You have 5 rounds to play. your score will show on this screen!';
+const screenSubtitle = document.createElement('div');
+screenSubtitle.classList.add('screenSubtitle');
+const title2 = document.createElement('p');
+title2.textContent = 'Choose your weapon!';
+const screenTitle = document.createElement('p');
+const separator = document.createElement('br');
+const scoreboard = document.createElement('p');
+
+
+//buttons
+
+const screenBtns = document.querySelector('div[class="btnsList"]');
+//Select all buttons and returns a nodelist
+const btnList = document.querySelectorAll('button[class="screenBtns"]');
+//Nodelists must be converted to arrays for us to be able to add events to them
+const btnListArr = Array.from(btnList);
+
+/*
+const replayButton = document.createElement('button')
+replayButton.innerText = "Replay";
+replayButton.classList.add('replayBtn')
+*/
+const replayButton = document.querySelector('button[class="replayBtn"]');
+
+
+
+
+function cleaningJob(){
+    gameBox.removeChild(screenBtns);
+    gameBox.removeChild(replayButton);
+}
+
+cleaningJob();
+
+
+//The function below remove the play button and append the screen, screen subtitle and start button
+function showGame(){
+
+    mainSelector.removeChild(btn);
+    gameBox.appendChild(screen);
+    screen.appendChild(title1);
+    gameBox.appendChild(screenSubtitle);
+    gameBox.appendChild(startBtn);
+
+}
+
+//The function below remove the start button and changes the screen's massage.
+function showGame2(){
+
+    gameBox.removeChild(startBtn);
+    screen.removeChild(title1);
+    screenSubtitle.appendChild(title2);
+    gameBox.appendChild(screenBtns);
+
+}
+
+//The function below generates a random choice for the computer's play
 function getComputerChoice () {
 
     let x = Math.floor((Math.random() * 3) + 1);
@@ -25,140 +88,93 @@ function getComputerChoice () {
 }
 
 
-//This function will compare my selection with the computer's.
 
-function playRound(playerSelection, a = computerSelection) {
-    
-    let b = playerSelection;
-    let refereeDecision = "";
-        
-    if (b == "Rock" && a == "Paper") {
-        refereeDecision = "pc";
-    } else if (b == "Paper" && a == "Rock") {
-        refereeDecision = "user";
-    }else if (b == "Rock" && a == "Scissors") {
-        refereeDecision = "user";
-    }else if (b == "Scissors" && a == "Rock") {
-        refereeDecision = "pc";
-    }else if (b == "Paper" && a == "Scissors") {
-        refereeDecision = "pc";
-    }else if (b == "Scissors" && a == "Paper") {
-        refereeDecision = "user";
-    }
-
-    return refereeDecision;
-
-}
-
-//The function below will decide who is going to be the winner.
-
-
-
-
-const mainSelector = document.querySelector('main');
-const btn = document.querySelector('#button');
-const gameBox = document.querySelector('#game');
-const screen = document.createElement('div');
-screen.classList.add('screen');
-const title1 = document.createElement('p');
-title1.textContent = 'You have 5 rounds to play. your score will show on this screen!';
-const startBtn = document.createElement('button');
-startBtn.textContent = "Start";
-startBtn.classList.add('startBtn');
-
-
-
-const screenSubtitle = document.createElement('div');
-screenSubtitle.classList.add('screenSubtitle');
-const title2 = document.createElement('p');
-title2.textContent = 'Choose your weapon!';
-const screenBtns = document.querySelector('div[class="btnsList"]');
-//buttons
-const screenBtn1 = document.querySelector('button[id="screenBtn1"]');
-const screenBtn2 = document.querySelector('button[id="screenBtn2"]');
-const screenBtn3 = document.querySelector('button[id="screenBtn3"]');
-
-//Iterate through all the buttons
-const btnList = document.querySelectorAll('button[class="screenBtns"]');
-const btnListArr = Array.from(btnList);
-
-
-
-
-
-function cleaningJob(){
-    gameBox.removeChild(screenBtns);
-}
-
-cleaningJob();
-
-
-
-function showGame(){
-
-    mainSelector.removeChild(btn);
-    gameBox.appendChild(screen);
-    screen.appendChild(title1);
-    gameBox.appendChild(screenSubtitle);
-    gameBox.appendChild(startBtn);
-
-}
-
-function showGame2(){
-
-    gameBox.removeChild(startBtn);
-    screen.removeChild(title1);
-    screenSubtitle.appendChild(title2);
-    gameBox.appendChild(screenBtns);
-
-}
-
-
-    let user = 0;
-    let pc = 0;
-    let playerSelection = "";
+let user = 0;
+let pc = 0;
+let draws = 0;
+let playerSelection = "";
 
 //This function compares the user selection and the computer selection
 function revelation(e){
+    
+    //The condition below cleans the screen on every click
+    if(user == 0 && pc == 0 && draws == 0){
+        screen.appendChild(screenTitle);
+        screen.appendChild(separator);
+        screen.appendChild(scoreboard);
+    }if(user > 0 || pc > 0 || draws > 0){
+        screenTitle.innerText = "";
+    }
 
     let computerSelection = getComputerChoice();
 
-    //console.log(e.target.innerText);
+
     if(e.target.innerText == "Rock" && computerSelection == "Paper"){
         pc++;
-        console.log("pc");
+        
+        screenTitle.innerText = "The PC won!";
+           
     } else if(e.target.innerText == "Paper" && computerSelection == "Rock"){
         user++;
-        console.log("user");
+       
+        screenTitle.innerText = "You won!";
+
     } else if(e.target.innerText == "Rock" && computerSelection == "Scissors"){
         user++;
-        console.log("user");
+
+        screenTitle.innerText = "You won!";
+
     } else if(e.target.innerText == "Paper" && computerSelection == "Scissors"){
         pc++;
-        console.log("pc");
+
+        screenTitle.innerText = "The PC won!";
+
     }else if(e.target.innerText == "Scissors" && computerSelection == "Paper"){
         user++;
-        console.log("user");
+
+        screenTitle.innerText = "You won!";
+        
     }else if(e.target.innerText == "Scissors" && computerSelection == "Rock"){
         pc++;
-        console.log("pc");
+
+        screenTitle.innerText = "The PC won!";
+
     }else if(e.target.innerText == computerSelection){
-        console.log('There is a draw!');
+        draws++;
+
+        screenTitle.innerText = "There is a draw!";
+
     }
     //Once one of the players get 5 points the buttons disappear
     if (user == 5) {
-        console.log("You won!");
+
+        screenTitle.innerText = "";
+        screenTitle.innerText = "You are the winner, look at the scoreboard:";
+        scoreboard.innerText = "You: "+ user +" PC: "+ pc;
         gameBox.removeChild(screenBtns);
+        gameBox.removeChild(screenSubtitle);
+        e.stopPropagation();
+        //Replay button
+        gameBox.appendChild(replayButton);
+
     }if (pc == 5) {
-        console.log("The PC won!");
+
+        screenTitle.innerText = "";
+        screenTitle.innerText = "The PC is the winner, look at the scoreboard:";
+        scoreboard.innerText = "You: "+ user +" PC: "+ pc;
         gameBox.removeChild(screenBtns);
+        gameBox.removeChild(screenSubtitle);
+        e.stopPropagation();
+        //Replay button
+        gameBox.appendChild(replayButton);
     }
+
+    e.stopPropagation();
     
-    //e.stopPropagation()
+    
+    
 
 }
-
-
 
 
 
@@ -166,8 +182,20 @@ function revelation(e){
 
 btn.addEventListener('click', showGame);
 startBtn.addEventListener('click', showGame2);
-screenBtn1.addEventListener('click', revelation);
-screenBtn2.addEventListener('click', revelation);
-screenBtn3.addEventListener('click', revelation);
+btnListArr.forEach(btn => btn.addEventListener('click', revelation))
+
+
+//refreshes the the page 
+function refreshBtn (){
+    location.reload();
+    /*If you are refreshing after an onclick then you'll need to return false directly after
+    https://stackoverflow.com/questions/3715047/how-to-reload-a-page-using-javascript
+    */
+    return false;
+}
+
+
+replayButton.addEventListener('click', refreshBtn)
+
 
 
